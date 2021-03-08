c2b18b7eb51eeed0213ad3bb821e75b8f0aa8d7c
Josh Hughes <josh@josh-hughes.com>
2021-03-07 21:28:05 -0600

    Some code cleanup for Set Review Date

--- SetReviewDate.omnifocusjs/Resources/SetReviewDate.js
+++ SetReviewDate.omnifocusjs/Resources/SetReviewDate.js
@@ -1,54 +1,54 @@
-var _ = function() {
-    const action = new PlugIn.Action(function() {
-
-        const folderIds = [];
-        const folderNames = [];
-        const buildFolderList = (folderGroup, prefix) => {
-            if (typeof folderGroup === 'undefined') {
-                folderGroup = folders;
-            }
-            if (typeof prefix === 'undefined') {
-                prefix = '';
-            }
-
-            if (folderGroup.length > 0) {
-                for (let i = 0; i < folderGroup.length; i++) {
-                    let fullPath = prefix + folderGroup[i].name;
-                    if (folderGroup[i].flattenedProjects.length > 0) {
-                        folderIds.push(folderGroup[i].id.primaryKey);
-                        folderNames.push('Folder: ' + fullPath);
-                    }
-                    buildFolderList(folderGroup[i].folders, fullPath + ' : ');
+(() => {
+    return new PlugIn.Action(() => {
+    
+        // Reusable filter to limit projects to active and on hold
+        const reviewProjectFilter = (projectObject) => {
+            return (projectObject.status === Project.Status.Active || projectObject.status === Project.Status.OnHold);
+        }
+        
+        // Recursively collect a list of folder ids and names that contain full paths
+        const folderIds = [],
+              folderNames = [];
+              
+        const buildFolderList = (folderList = folders, previousFolderHierarchyName = '') => {
+            folderList.forEach((folderObject) => {
+                const fullName = previousFolderHierarchyName + folderObject.name;
+                
+                // Only add the folder if it has active/on hold projects within
+                if (folderObject.flattenedProjects.filter(reviewProjectFilter).length > 0) {
+                    folderIds.push(folderObject.id.primaryKey);
+                    folderNames.push('Folder: ' + fullName);
                 }
-            }
+                
+                buildFolderList(folderObject.folders, fullName + ' : ');
+            });
         };
         buildFolderList();
 
-        const datePickerForm = new Form();
-        const dateFormatter = Formatter.Date.withStyle(Formatter.Date.Style.Short, null);
-        const datePickerField = new Form.Field.Date('reviewDate', 'New Review Date', new Date(), dateFormatter);
-        datePickerForm.addField(datePickerField);
+        // Setup form and date field
+        const reviewDateForm = new Form();
+              dateFormatter = Formatter.Date.withStyle(Formatter.Date.Style.Short, null);
+              datePickerField = new Form.Field.Date('reviewDate', 'New Review Date', new Date(), dateFormatter);
+              
+        reviewDateForm.addField(datePickerField);
+        
+        // Add folder list to the options if there are any
         if (folderIds.length > 0) {
             const selectedFolderField = new Form.Field.Option('selectedFolder', 'Set Date for', folderIds, folderNames);
             selectedFolderField.allowsNull = true;
             selectedFolderField.nullOptionTitle = 'All Projects';
-            datePickerForm.addField(selectedFolderField);
+            reviewDateForm.addField(selectedFolderField);
         }
-        const datePickerPromise = datePickerForm.show('Choose a New Review Date', 'Select Date');
         
-        datePickerPromise.then(function(form) {
-            let container = flattenedProjects;
-            if (form.values.selectedFolder) {
-                container = Folder.byIdentifier(form.values.selectedFolder).flattenedProjects;
-            }
-            container.forEach((project) => {
-                if (project.status === Project.Status.Active || project.status === Project.Status.OnHold) {
-                    project.nextReviewDate = form.values.reviewDate;
-                }
+        // Show the form and set the apply the new date when fired
+        const reviewDateFormPromise = reviewDateForm.show('Choose a New Review Date', 'Select Date');
+        
+        reviewDateFormPromise.then(function(form) {
+            const projectList = (form.values.selectedFolder ? Folder.byIdentifier(form.values.selectedFolder).flattenedProjects : flattenedProjects); 
+            
+            projectList.filter(reviewProjectFilter).forEach((projectObject) => {
+                projectObject.nextReviewDate = form.values.reviewDate;
             });
         });
     });
-
-    return action;
-}();
-_;
+})();
